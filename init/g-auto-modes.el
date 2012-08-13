07bb6ae543364677f12fb5e9844973c8bc554e4e
George Ogata <george.ogata@gmail.com>
2012-08-12 22:31:46 -0400

    Add some automodes.

--- init/g-auto-modes.el
+++ init/g-auto-modes.el
@@ -19,13 +19,13 @@ any file with that extension should be loaded with MODE."
 
 (auto-mode 'c++-mode 'ut)
 (auto-mode 'css-mode 'scss)
-(auto-mode 'coffee-mode 'coffee)
+(auto-mode 'coffee-mode 'coffee '_coffee "Cakefile")
 (auto-mode 'objc-mode 'm)
 (auto-mode 'd-mode 'd 'di)
 (auto-mode 'ruby-mode 'rb 'rjs 'rake 'rsel 'god 'to_json "[Rr]akefile$" "Capfile" "Gemfile" ".autotest")
 (auto-mode 'feature-mode 'feature)
 (auto-mode 'html-mode 'html.erb)
-(auto-mode 'javascript-mode 'js 'htc)
+(auto-mode 'js-mode 'js 'json '_js 'htc "Jakefile")
 (auto-mode 'yaml-mode 'yml 'yaml 'yml.sample)
 (auto-mode 'html-mode 'html 'htm 'rhtml)
 (auto-mode 'haml-mode 'haml)
@@ -35,7 +35,9 @@ any file with that extension should be loaded with MODE."
 (auto-mode 'pman-mode "^/?pman_trace")
 (auto-mode 'metafont-mode "\\.mf\\'")
 (auto-mode 'metapost-mode "\\.mp\\'")
-(auto-mode 'conf-mode 'cnf)  ; e.g., my.cnf
+(auto-mode 'conf-mode 'cnf ".gitconfig")  ; e.g., my.cnf
 (auto-mode 'scala-mode 'scala)
+
+(interpreter-mode 'js-mode "node")
 
 (provide 'g-auto-modes)
