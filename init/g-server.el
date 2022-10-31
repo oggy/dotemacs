0f06f83925fe2768eb457eb207dfa75948558df5
George Ogata <george.ogata@gmail.com>
2022-10-31 05:03:54 -0400

    Assume a single root per editor: g-start-dir

--- init/g-server.el
+++ init/g-server.el
@@ -2,38 +2,33 @@
 
 ;; Maintain a separate server per project.
 ;;
-;; We write the project root(s) to a file so that bin/emacs can open
-;; files using the right server.
+;; We write g-start-dir to a file so that bin/open can open files using the
+;; right server.
 
 (setq server-name (format "server-%d" (emacs-pid)))
 
-(defvar g-server-roots-path (rc-path (format "servers/%s" server-name))
+(defvar g-server-path (rc-path (format "servers/%s" server-name))
   "Path to our server file.")
 
-(defvar g-server-roots-cache nil
-  "The last value we wrote to the server file.")
-
-(defun g-server-write-roots (&optional force)
-  "Write the server file if the project roots have changed (or if FORCE)."
-  (let* ((dir (file-name-directory g-server-roots-path))
-         (roots (g-project-roots)))
-    (when (or force (not (equal roots g-server-roots-cache)))
-      (make-directory dir t)
-      (with-temp-buffer
-        (mapc (lambda (path) (insert path) (insert "\n")) roots)
-        (write-file g-server-roots-path)
-        (setq g-server-roots-cache roots)
-        (kill-buffer)))))
+(defun g-server-write-file ()
+  "Write the server file."
+  (let* ((dir (file-name-directory g-server-path))
+         (roots g-start-dir))
+    (make-directory dir t)
+    (with-temp-buffer
+      (insert g-start-dir "\n")
+      (write-file g-server-path)
+      (kill-buffer))))
 
 (defun g-server-delete-roots ()
   "Delete the roots file."
-  (when (file-exists-p g-server-roots-path)
-    (delete-file g-server-roots-path)))
+  (when (file-exists-p g-server-path)
+    (delete-file g-server-path)))
 
-(add-hook 'buffer-list-update-hook 'g-server-write-roots)
 (add-hook 'kill-emacs-hook 'g-server-delete-roots)
 
-(unless server-process
-  (server-start))
+(when (and g-start-dir (null server-process))
+  (server-start)
+  (g-server-write-file))
 
 (provide 'g-server)
